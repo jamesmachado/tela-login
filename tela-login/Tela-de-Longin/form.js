294bc91e45bba1c4c2e9c07b8f4f5d740fed84fa
jamesmachado <Jamesmavhado@gmail.com>
2023-02-07 12:47:41 -0300

    first commit

---
+++ tela-login/Tela-de-Longin/form.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+    $("#do_login").click(function(){
+        closeLoginInfo();
+        $(this).parent().find("span").css("display","nome");
+        $(this).parent().find("span").removeClass("i-save");
+        $(this).parent().find("span").removeClass("i-warning");
+        $(this).parent().find("span").removeClass("i-close");
+
+        var proceed = true;
+        $("#login_form input").each(function(){
+
+            if(!$.trim($(this).val())){
+                $(this).parent().find('span').addclass("i-warning");
+                $(this).parent().find('span').css("display","block");
+                proceed = false;
+            }
+        });
+        if(proceed)
+        {
+            $(this).parent().find('span').addclass("i-save");
+            $(this).parent().find('span').css("display","block");
+        }
+    });
+
+   openLoginInfo();
+   setTimeout(closeLoginInfo, 1000); 
+});
+
+function openLoginInfo(){
+    $(document).ready(function(){
+         $('.b-form').css("opacity","0.01");
+        $('.box-form').css("left","-37%");
+        $('.box-info').css("right","-37%");
+    });
+}
+function closeLoginInfo(){
+    $(document).ready(function(){
+        $(',b-form').css("opacity","1");
+        $('.box-form').css("left","0px");
+        $(',box-info').css("right","-5px");
+    });
+}
+$(window).on('resize', function(){
+    closeLoginInfo()
+});
